8f0539371237e47215d9284dcdd382f336def7c4
malvika1010 <144083912+malvika1010@users.noreply.github.com>
2023-09-04 20:49:33 -0700

    Update app.py - creating task object and naming DB

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task_db_mn.db'  # SQLite database URL
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.id}>'
+
 
 @app.route('/')
 def hello_world():
